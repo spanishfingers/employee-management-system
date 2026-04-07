c109ac22431cec73ef64057a0f5bdb2b3d56cbd8
Ashutosh Upadhyay <ashutoshupadhyay.iimun@gmail.com>
2026-04-07 22:08:38 +0530

    ready for deployment

--- config.py
+++ config.py
@@ -1,7 +1,19 @@
+# import os
+
+# class Config:
+#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'payroll-secret-key-2024'
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+#         'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance', 'project.db')
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'payroll-secret-key-2024'
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance', 'project.db')
+        'sqlite:///' + os.path.join(basedir, 'instance', 'project.db')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
